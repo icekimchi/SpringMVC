a09538d395fa968f2dfa6775253feebb4fd0830c
icekimchi <pjh010828@gmail.com>
2025-03-04 23:57:27 +0900

    feat: 메시지 국제화 예외 테스트 케이스 추가

--- message/src/test/java/hello/itemservice/message/MessageSourceTest.java
+++ message/src/test/java/hello/itemservice/message/MessageSourceTest.java
@@ -5,8 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.MessageSource;
+import org.springframework.context.NoSuchMessageException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
 public class MessageSourceTest {
@@ -18,5 +20,12 @@ public class MessageSourceTest {
     void helloMessage(){
         String result = ms.getMessage("hello", null, null);
         assertThat(result).isEqualTo("안녕");
+    }
+
+    @Test
+    void notFoundMessageCode(){
+        assertThatThrownBy(() -> ms.getMessage("no_code", null, null))
+                .isInstanceOf(NoSuchMessageException.class);
+
     }
 }
